ef2cc0f9c679b16a177c2bc9a1d8b0c0b066575d
KKHoon210417 <zozo1591@naver.com>
2022-03-21 14:09:11 +0900

    add : JAVA1 ClassApp

---
+++ 생활코딩/ClassApp/src/ClassApp.java
@@ -0,0 +1,7 @@
+public class ClassApp {
+    public static void main(String[] args) {
+        System.out.println(Math.PI); // 파이
+        System.out.println(Math.floor(1.6)); // 내림
+        System.out.println(Math.ceil(1.6)); // 올림
+    }
+}
